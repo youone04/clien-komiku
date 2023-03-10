8384b9211e7982abf0dfee03d6e0ba29d45e6baa
youone04 <yudi.gunaone87@gmail.com>
2023-03-10 08:18:52 +0700

    request api home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 function Home() {
+
+    const [dataHome , setDataHome] = useState({
+        loading: true,
+        data: []
+    })
+
+    useEffect(() => {
+        getKomikHome();
+
+    },[]);
+
+    const getKomikHome = async() => {
+        try{
+           const data = await fetch('http://192.168.253.39:3000/api/terbaru', {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                  },
+            });
+
+            const dataKomik = await data.json();
+            setDataHome((prev) => ({
+                ...prev,
+                loading: false,
+                data: dataKomik
+            }))
+
+        }catch(e){
+            throw e
+        }
+    }    
   return (
     <View style={styles.container}>
         <Text>Home</Text>
